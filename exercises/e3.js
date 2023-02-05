af7a5e60c395203464dcb15583fe044115cc6030
Steven Brcak <sbrcak@live.com>
2023-02-05 00:48:49 -0800

    Complete e3.js

--- exercises/e3.js
+++ exercises/e3.js
@@ -6,6 +6,8 @@ import { data } from "../data/data";
 
 export function getAllAverageTemperatures(data) {
   // Your code goes here...
+  const planetTemp = data.planets.map(planet => {return planet.avgTemp});
+  return planetTemp;
 }
 
 
