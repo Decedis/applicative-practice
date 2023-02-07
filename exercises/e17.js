c621e2945e442e42dc4cdf4e22e06423752a521a
Steven Brcak <sbrcak@live.com>
2023-02-07 02:47:40 -0800

    Complete e17.js. However, in the testing phase it's stated that 4 tests were skipped.

--- exercises/e17.js
+++ exercises/e17.js
@@ -1,3 +1,41 @@
-export function minBy(array, cb) {}
+/*
+a minBy function should
+• Take an array of things
+• return a the element where the callback function on that element yields the lowest value
 
-export function maxBy(array, cb) {}
+a maxBy function should
+• Take an array of things
+• return a the element where the callback function on that element yields the highest value
+*/
+
+export function minBy(array, cb) {
+    if(array.length > 0){
+        let min = cb(array[0]);
+        for (let i = array.length; i > 0; i--){
+            if(min > cb(array[i - 1])){
+                min = cb(array[i - 1]);
+            }
+        }
+        let returnVal = array.filter(person => {return person.age === min});
+        return returnVal[0];
+    }
+    else {
+        return undefined;
+    }
+}
+
+export function maxBy(array, cb) {
+    if(array.length > 0){
+        let max = cb(array[0]);
+        for (let i = array.length; i > 0; i--){
+            if(max < cb(array[i - 1])){
+                max = cb(array[i - 1]);
+            }
+        }
+        let returnVal = array.filter(person => {return person.age === max});
+        return returnVal[0];
+    }
+    else {
+        return undefined;
+    }
+}
