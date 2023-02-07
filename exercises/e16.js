766acea00bef7c624f4c63e9b11044b808e6f640
Steven Brcak <sbrcak@live.com>
2023-02-07 01:07:14 -0800

    Complete e16.js. Thanks again Andrey

--- exercises/e16.js
+++ exercises/e16.js
@@ -1,3 +1,18 @@
 // see e16.md
-
-export function find(array, callback) {}
+/* 
+[
+    { v: false, id: 1 },
+    { v: false, id: 2 },
+    { v: false, id: 3 },
+]
+*/
+export function find(array, callback) {
+    let resultVal;
+    for(let search in array){
+        if(callback(array[search])){
+            resultVal = array[search];
+            break;
+        }
+    }
+    return resultVal;
+}
